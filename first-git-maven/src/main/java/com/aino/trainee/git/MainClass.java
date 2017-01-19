05d37ecb446c067e65db0b11aba539a95ea3ab30
iternia <iternia@PC-4>
2017-01-19 10:27:22 +0530

    hi

--- first-git-maven/src/main/java/com/aino/trainee/git/MainClass.java
+++ first-git-maven/src/main/java/com/aino/trainee/git/MainClass.java
@@ -5,7 +5,7 @@ public class MainClass {
 	public static void main(String args[]){
 		
 		System.out.println("hello deepali");
-		System.out.println("i love you");
+	//	System.out.println("how are you");
 	}
 	
 }
